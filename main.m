9c1a33448b7bed8cc43e35c156d7f7e43bcc41e6
mkofler96 <rasta.kof@gmail.com>
2020-05-30 13:22:24 +0200

    Geschwindigkeitsregler
    Geschwindigkeitsregler in Simulink Modell eingebaut
    schönere Plots
    Parameter file angepasst

--- main.m
+++ main.m
@@ -1,32 +1,46 @@
-clc; clear all; close all;
+clear all; close all;
 
 %% parameters
-run('parameters.m');
+run('parameters_v2.m');
 
 %% simulation
-simout = sim('lunar_lander.slx');
+simout = sim('lunar_lander_v2.slx')
 
 %% post process
 
 sim.s_Lunar = simout.s_Lunar.signals.values(:,1);
 sim.time = simout.s_Lunar.time;
-sim.m_Lunar_aktuell = simout.m_Lunar_aktuell.signals.values(:,1);
-sim.g_Mond = simout.g_Mond.signals.values(:,1);
+% sim.m_Lunar_aktuell = simout.m_Lunar_aktuell.signals.values(:,1);
+% sim.g_Mond = simout.g_Mond.signals.values(:,1);
+sim.v_Lunar = simout.v_Lunar.signals.values;
+sim.m_Fuel = simout.m_Fuel.signals.values;
 
-figure(1)
+
+figure('Name','Apollo 11 Mondlandung','numbertitle','off')
 subplot(3,1,1)
 plot(sim.time, sim.s_Lunar);
 title('Height above Moon Surface');
-title('Height [m]');
+xlabel('Time [s]');
+ylabel('Height [m]');
+ylim([-200 s_Lunar+200])
+grid on
 
 subplot(3,1,2);
-plot(sim.time, sim.m_Lunar_aktuell/10);
-title('Mass Lunar Lander')
-ylabel('Mass [t]');
+plot(sim.time, sim.v_Lunar);
+title('Velocity Lunar Lander')
+xlabel('Time [s]');
+ylabel('Velocity [m/s]');
+ylim([min(sim.v_Lunar)-2 0])
+grid on
 
 subplot(3,1,3);
-plot(sim.time, sim.g_Mond);
+plot(sim.time, sim.m_Fuel);
 xlabel('Time [s]');
-ylabel('Grav. Acceleration [m]');
-title('Moon Gravitational Acceleration');
+ylabel('Mass [kg]');
+title('Mass of Fuel');
 sgtitle(sprintf('Lunar Landing - Entry Velocity: %d m/s', v_Lunar)); 
+ylim([-400 m_Tank+400])
+grid on
+
+
+
--- parameters.m
+++ parameters.m
@@ -4,11 +4,11 @@ m_Lunar_leer    = 2134; % kg
 m_Impuls        = 140; %s
 m_Tank          = 8200; % kg
 r_Mond          = 1737000; % m
-m_Mond           = 7.346e22; % kg
+m_Mond          = 7.346e22; % kg
 G               = 6.6743e-11; % m^3/(kg*s^2)
 
 % todo: choose s_Lunar
-s_Lunar         = 1000; % m
+s_Lunar         = 3200; % m
 
 % todo: choose v_Luaner (2 dimensionen? radial und tangential?)
-v_Lunar         = -60; %m/s
+v_Lunar         = -20; %m/s
